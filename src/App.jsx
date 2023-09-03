ad5767b5f8b02e2c949670ac59bbb7a8881957d3
kacperjanus <kacperjanus8@gmail.com>
2023-09-03 18:59:24 +0200

    Installing Prettier formatter and fixing formatting

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 const queryClient = new QueryClient();
 
 function App() {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<ReactQueryDevtools initialIsOpen={false} />
-			<BrowserRouter>
-				<Routes>
-					<Route path="homepage" element="" />
-					<Route path="*" element="" />
-				</Routes>
-			</BrowserRouter>
-			<div className="text-3xl">Hello React</div>;
-		</QueryClientProvider>
-	);
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="homepage" element="" />
+                    <Route path="*" element="" />
+                </Routes>
+            </BrowserRouter>
+            <div className="text-3xl">Hello React</div>;
+        </QueryClientProvider>
+    );
 }
 
 export default App;
