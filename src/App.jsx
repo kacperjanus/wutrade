2e1d2e7a1e24290dd9c695028d38c6b92a6bb3b7
kacperjanus <kacperjanus8@gmail.com>
2023-09-03 18:57:01 +0200

    Installing React Router library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
@@ -7,6 +8,12 @@ function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<ReactQueryDevtools initialIsOpen={false} />
+			<BrowserRouter>
+				<Routes>
+					<Route path="homepage" element="" />
+					<Route path="*" element="" />
+				</Routes>
+			</BrowserRouter>
 			<div className="text-3xl">Hello React</div>;
 		</QueryClientProvider>
 	);
