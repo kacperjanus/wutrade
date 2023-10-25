098b52cba9f08a69fae8e51ab5355209622a4914
kacperjanus <kacperjanus8@gmail.com>
2023-10-26 00:38:30 +0200

    Added login and register forms to the respective pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,32 @@
+import { useForm } from "react-hook-form";
+
 function Login() {
-    return <div>Log in</div>;
+    const { register, handleSubmit } = useForm();
+
+    function onSubmit(data) {
+        if (!data.email || !data.password) return;
+        console.log(data);
+    }
+
+    return (
+        <>
+            <div>Log in</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Email</label>
+                <input
+                    className="border-solid border-2"
+                    {...register("email")}
+                />
+                <label>Password</label>
+                <input
+                    className="border-solid border-2"
+                    type="password"
+                    {...register("password")}
+                />
+                <input type="submit" />
+            </form>
+        </>
+    );
 }
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,38 @@
+import { useForm } from "react-hook-form";
+
 function Register() {
-    return <div>Register</div>;
+    const { register, handleSubmit } = useForm();
+
+    function onSubmit(data) {
+        if (!data.email || !data.password || !data.confirmPassword) return;
+        console.log(data);
+    }
+
+    return (
+        <>
+            <div>Register</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Email</label>
+                <input
+                    className="border-solid border-2"
+                    {...register("email")}
+                />
+                <label>Password</label>
+                <input
+                    className="border-solid border-2"
+                    type="password"
+                    {...register("password")}
+                />
+                <label>Confirm password</label>
+                <input
+                    className="border-solid border-2"
+                    type="password"
+                    {...register("confirmPassword")}
+                />
+                <input type="submit" />
+            </form>
+        </>
+    );
 }
 
 export default Register;
