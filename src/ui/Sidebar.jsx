d069f11bd824b53a746df6d93b4877883b6a9c89
kacperjanus <kacperjanus8@gmail.com>
2023-11-07 21:39:53 +0100

    Adding navigation links to the sidebar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,9 +1,11 @@
 import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 function Sidebar() {
     return (
         <aside className="bg-yellow-500 py-[3.2rem] px-[2.4rem] border-r-1 flex flex-col gap-[3.2rem] row-[-1/1]">
             <Logo />
+            <MainNav />
         </aside>
     );
 }
