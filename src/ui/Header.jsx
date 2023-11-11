c12cc38565831391ae96c71a66c5b7aafc0e8bb6
kacperjanus <kacperjanus8@gmail.com>
2023-11-11 20:34:24 +0100

    Exported Search Bar into separate component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,6 +4,7 @@ import { getCurrentUser } from "../services/apiAuth";
 import PrimaryButton from "./PrimaryButton";
 import { formatCurrency } from "../utils/helpers";
 import Spinner from "./Spinner";
+import SeachBar from "./SearchBar";
 
 function Header() {
     const { signOut, isLoading } = useSignOut();
@@ -14,10 +15,7 @@ function Header() {
         <header className="bg-red-500 py-[1.2rem] px-[3.6rem] gap-[2.4rem] border-b-1 items-center justify-end grid grid-cols-[1fr,1fr,auto,auto]">
             {isLoading ? <Spinner /> : ""}
             <h1>Hello {data?.firstName}</h1>
-            <input
-                className="w-60 focus:w-80 transition-width ease-in-out duration-300 justify-self-end rounded-full px-3 py-2"
-                placeholder="Search"
-            ></input>
+            <SeachBar />
             <h1>Balance: {formatCurrency(data?.balance)}</h1>
             <PrimaryButton onClick={() => signOut()}>Log out</PrimaryButton>
         </header>
---
+++ src/ui/SearchBar.jsx
@@ -0,0 +1,10 @@
+function SeachBar() {
+    return (
+        <input
+            className="w-40 focus:w-60 transition-width ease-in-out duration-300 justify-self-end rounded-full px-3 py-2 outline-none"
+            placeholder="Search"
+        ></input>
+    );
+}
+
+export default SeachBar;
