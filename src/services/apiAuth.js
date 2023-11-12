d535c630d8471fb07093eeca61dbf533bc2ccf4f
kacperjanus <kacperjanus8@gmail.com>
2023-11-12 16:43:53 +0100

    Changed the way user's metadata is stored

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,3 +1,4 @@
+import { INITIAL_BALANCE } from "../utils/constants";
 import supabase from "./supabase";
 
 export async function signOutUser() {
@@ -7,34 +8,18 @@ export async function signOutUser() {
 
 export async function signInWithEmail({ email, password }) {
     //Authenticate user with Supabase
-    const { data: signInData, error: signInError } =
-        await supabase.auth.signInWithPassword({
+    const { data, error: signInError } = await supabase.auth.signInWithPassword(
+        {
             email: email,
             password: password,
-        });
+        }
+    );
 
     //Check if login was successful
     if (signInError) throw new Error(signInError.message);
 
-    //Get user data from user_data table in the database
-    const { data: userData, error: userDataError } = await supabase
-        .from("user_data")
-        .select("*")
-        .eq("id", signInData.user.id)
-        .single();
-
-    if (userDataError) throw new Error(userDataError.message);
-
-    //Merge the data
-    const data = {
-        balance: userData.balance,
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        ...signInData.user,
-    };
-
     //Return logged in user's data
-    return data;
+    return data.user;
 }
 
 export async function signUpWithEmail({
@@ -44,40 +29,22 @@ export async function signUpWithEmail({
     password,
 }) {
     //Sign up with Supabase
-    const { data: signUpData, error: signUpError } = await supabase.auth.signUp(
-        {
-            email: email,
-            password: password,
-        }
-    );
+    const { data, error: signUpError } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        options: {
+            data: {
+                firstName,
+                lastName,
+                balance: INITIAL_BALANCE,
+            },
+        },
+    });
     //Check if login was successful
     if (signUpError) throw new Error(signUpError.message);
 
-    //Create user_data record
-    const {
-        data: [userData],
-        error: userDataError,
-    } = await supabase
-        .from("user_data")
-        .insert([
-            { id: signUpData.user.id, firstName, lastName, balance: 200000 },
-        ])
-        .select();
-
-    //Check if creating was successful
-    if (userDataError) throw new Error(userDataError.message);
-
-    console.log(userData);
-    //Join data
-    const data = {
-        balance: userData.balance,
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        ...signUpData.user,
-    };
-
     //Return registered user's data
-    return data;
+    return data.user;
 }
 
 export async function getCurrentUser() {
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -14,9 +14,9 @@ function Header() {
     return (
         <header className="bg-red-500 py-[1.2rem] px-[3.6rem] gap-[2.4rem] border-b-1 items-center justify-end grid grid-cols-[1fr,1fr,auto,auto]">
             {isLoading ? <Spinner /> : ""}
-            <h1>Hello {data?.firstName}</h1>
+            <h1>Hello {data?.user_metadata.firstName}</h1>
             <SeachBar />
-            <h1>Balance: {formatCurrency(data?.balance)}</h1>
+            <h1>Balance: {formatCurrency(data?.user_metadata.balance)}</h1>
             <PrimaryButton onClick={() => signOut()}>Log out</PrimaryButton>
         </header>
     );
