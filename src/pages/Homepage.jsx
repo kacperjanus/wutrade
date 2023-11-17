57d598bed9adce04fd9969d3bbad73069dd07b0a
kacperjanus <kacperjanus8@gmail.com>
2023-11-17 23:08:30 +0100

    Changed style of the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,20 +2,21 @@ import { Link } from "react-router-dom";
 
 function Homepage() {
     return (
-        <div>
-            <h1 className="flex justify-center">WUTrade</h1>
-            <h2 className="flex justify-center mb-10">
-                Your Virtual Stock Market Application
-            </h2>
-            <Link
-                className="flex justify-center bg-yellow-500 p-4"
-                to="/register"
-            >
-                Register
-            </Link>
-            <Link className="flex justify-center bg-orange-500 p-4" to="/login">
-                Log in
-            </Link>
+        <div className="flex flex-row">
+            <div className="bg-black flex flex-col h-screen w-full text-white justify-center items-center ">
+                <h2 className="text-3xl">WUTrade</h2>
+                <h2 className="text-white">
+                    Your Virtual Stock Market Application
+                </h2>
+            </div>
+            <div className="bg-[#282828] h-screen w-full flex flex-col justify-center items-center">
+                <Link
+                    className="flex justify-center bg-sky-400 text-white hover:bg-sky-600 hover:opacity-100 py-4 px-20 mx-16 rounded-full transition duration-500 ease-in-out"
+                    to="/register"
+                >
+                    Start investing
+                </Link>
+            </div>
         </div>
     );
 }
