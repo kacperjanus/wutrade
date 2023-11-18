a5bf0c808cfc510f872974485d3b8bcd050ab9d1
kacperjanus <kacperjanus8@gmail.com>
2023-11-18 19:08:34 +0100

    Introduced unified content boxes for application pages

---
+++ src/ui/ContentBox.jsx
@@ -0,0 +1,9 @@
+function ContentBox({ children }) {
+    return (
+        <div className="bg-[#282828] h-auto w-full mx-aut py-[30px] px-20">
+            {children}
+        </div>
+    );
+}
+
+export default ContentBox;
