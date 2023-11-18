1be9730a45737fc1351095181588010273893198
kacperjanus <kacperjanus8@gmail.com>
2023-11-18 19:46:53 +0100

    Added ability to assign additional Tailwind classes

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,11 @@
-function Button({ onClick, type, children }) {
+function Button({ onClick, type, className, children }) {
     return (
         <button
             className={[
                 "rounded-md px-2 py-1 m-2 text-white",
                 type === "primary" ? "bg-blue-400" : "",
                 type === "secondary" ? "bg-blue-200 bg-opacity-30" : "",
+                className,
             ].join(" ")}
             onClick={onClick}
         >
