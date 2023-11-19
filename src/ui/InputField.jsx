73341150f8aae89a02c97138ccc6f43dd2acd2ee
kacperjanus <kacperjanus8@gmail.com>
2023-11-19 14:30:38 +0100

    Changed color

--- src/ui/InputField.jsx
+++ src/ui/InputField.jsx
@@ -9,7 +9,7 @@ function InputField({
         <div className="flex flex-col">
             <label className="ml-2 text-xl">{label}</label>
             <input
-                className="bg-slate-600 focus:outline-0 bg-opacity-50 rounded-full px-4 py-2 focus:shadow-lg focus:shadow-sky-300/20"
+                className="bg-slate-700 focus:outline-0 bg-opacity-50 rounded-full px-4 py-2 focus:shadow-lg focus:shadow-sky-300/20"
                 defaultValue={defaultValue}
                 type={type}
                 {...registerFunction(fieldName)}
