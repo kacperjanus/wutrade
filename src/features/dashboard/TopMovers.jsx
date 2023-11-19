b59855ef3f414ac1672a0cc6f3c34e8e47cee906
kacperjanus <kacperjanus8@gmail.com>
2023-11-20 00:21:51 +0100

    Code refactoring

--- src/features/dashboard/TopMovers.jsx
+++ src/features/dashboard/TopMovers.jsx
@@ -1,69 +1,49 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import ContentBox from "../../ui/ContentBox";
 import { useTopMovers } from "./useTopMovers";
-import { useQueryClient } from "@tanstack/react-query";
 import FullSpinner from "../../ui/FullSpinner";
 import TopMover from "./TopMover";
 
 function TopMovers() {
-    const { getTopMovers, isLoading: isGettingTopMovers } = useTopMovers();
-    const queryClient = useQueryClient();
+    const [gainers, setGainers] = useState(false);
 
-    const data = queryClient.getQueryData(["topMovers"]);
+    // Get data from React Query
+    const { data, isLoading: isGettingTopMovers } = useTopMovers();
 
-    // useEffect(() => getTopMovers(), [getTopMovers]);
+    // Get TOP5 gainers or losers
+    const displayData = gainers
+        ? data?.top_gainers?.slice(0, 5)
+        : data?.top_losers?.slice(0, 5);
 
     return isGettingTopMovers ? (
         <FullSpinner />
     ) : (
         <ContentBox>
-            <h1>Top Movers/Losers</h1>
-            <div className="flex gap-5">
-                <ul>
-                    <TopMover
-                        ticker={data?.top_gainers[0].ticker}
-                        change_amount={data?.top_gainers[0].change_amount}
-                    />
-                    <TopMover
-                        ticker={data?.top_gainers[1].ticker}
-                        change_amount={data?.top_gainers[1].change_amount}
-                    />
-                    <TopMover
-                        ticker={data?.top_gainers[2].ticker}
-                        change_amount={data?.top_gainers[2].change_amount}
-                    />
-                    <TopMover
-                        ticker={data?.top_gainers[3].ticker}
-                        change_amount={data?.top_gainers[3].change_amount}
-                    />
-                    <TopMover
-                        ticker={data?.top_gainers[4].ticker}
-                        change_amount={data?.top_gainers[4].change_amount}
-                    />
-                </ul>
-                <ul>
-                    <TopMover
-                        ticker={data?.top_losers[0].ticker}
-                        change_amount={data?.top_losers[0].change_amount}
-                    />
-                    <TopMover
-                        ticker={data?.top_losers[1].ticker}
-                        change_amount={data?.top_losers[1].change_amount}
-                    />
-                    <TopMover
-                        ticker={data?.top_losers[2].ticker}
-                        change_amount={data?.top_losers[2].change_amount}
-                    />
-                    <TopMover
-                        ticker={data?.top_losers[3].ticker}
-                        change_amount={data?.top_losers[3].change_amount}
-                    />
-                    <TopMover
-                        ticker={data?.top_losers[4].ticker}
-                        change_amount={data?.top_losers[4].change_amount}
-                    />
-                </ul>
+            <div className="flex flex-row justify-between items-center">
+                <h1>Top {gainers ? "Gainers" : "Losers"}</h1>
+                <button
+                    type="secondary"
+                    value={gainers}
+                    onClick={() => setGainers((gainers) => !gainers)}
+                >
+                    See {gainers ? "Losers" : "Gainers"}
+                </button>
             </div>
+            {displayData ? (
+                <ul>
+                    {displayData?.map((mover, i) => (
+                        <li>
+                            <TopMover
+                                position={i + 1}
+                                ticker={mover.ticker}
+                                change_amount={mover.change_amount}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                "No data to display"
+            )}
         </ContentBox>
     );
 }
