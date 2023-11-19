5c295f4fa5e8d8a9130490288e133957af53f8a7
kacperjanus <kacperjanus8@gmail.com>
2023-11-19 15:23:15 +0100

    Added TopMovers component and described page layout in comments

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,18 @@
-import { useEffect } from "react";
-import { useTopMovers } from "../features/stocks/useTopMovers";
-import { getTopMovers } from "../services/apiStockData";
 import PageLayout from "../ui/PageLayout";
 import SectionHeader from "../ui/SectionHeader";
 import FullSpinner from "../ui/FullSpinner";
+import TopMovers from "../features/dashboard/TopMovers";
 
 function Dashboard() {
-    const { getTopMovers, isLoading: isGettingTopMovers } = useTopMovers();
+    // 1. Portfolio value chart
+    // 2. Top Movers/Losers and Most Traded
+    // 3. Watchlist preview with button to go to /dashboard
+    // 4. Leaderboard preview
 
-    //useEffect(() => getTopMovers(), [getTopMovers]);
-
-    return isGettingTopMovers ? (
-        <FullSpinner />
-    ) : (
+    return (
         <PageLayout>
-            <SectionHeader>Dashboard</SectionHeader>;
+            <SectionHeader>Dashboard</SectionHeader>
+            <TopMovers />
         </PageLayout>
     );
 }
