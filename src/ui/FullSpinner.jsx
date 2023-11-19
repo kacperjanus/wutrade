fc4bbda46b6b9b922b28fc12f0a20712e55d12df
kacperjanus <kacperjanus8@gmail.com>
2023-11-19 14:34:24 +0100

    Renamed Spinner to FullSpinner to distinguish from other spinners

--- src/ui/FullSpinner.jsx
+++ src/ui/FullSpinner.jsx
@@ -1,4 +1,4 @@
-function Spinner() {
+function FullSpinner() {
     return (
         <div className="absolute bg-opacity-[0.2] top-0 right-0 left-0 bottom-0 bg-white">
             <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600 absolute top-2/4 left-2/4" />
@@ -6,4 +6,4 @@ function Spinner() {
     );
 }
 
-export default Spinner;
+export default FullSpinner;
