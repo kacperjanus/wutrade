92f91fb5bbc5fcf245fa1a92a1ced6d3b1ba4280
kacperjanus <kacperjanus8@gmail.com>
2023-11-19 14:30:58 +0100

    Added copyright footer

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -6,6 +6,9 @@ function Sidebar() {
         <aside className="bg-[#282828] py-[3.2rem] px-[2.4rem] border-r-1 flex flex-col gap-[3.2rem] row-[-1/1]">
             <Logo />
             <MainNav />
+            <h1 className="text-white text-sm text-opacity-30 text-center mt-auto">
+                Copyright © 2023 Kacper Janus. All rights reserved.
+            </h1>
         </aside>
     );
 }
