56e74d9c8da6ca9a1af4ea69abe5b74f687fb99d
kacperjanus <kacperjanus8@gmail.com>
2023-11-19 14:29:41 +0100

    Added color transition and disabled state

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,16 @@
-function Button({ onClick, type, className, children }) {
+function Button({ onClick, type, className, disabled, children }) {
     return (
         <button
             className={[
                 "rounded-md px-2 py-1 m-2 text-white",
                 type === "primary" ? "bg-blue-400" : "",
-                type === "secondary" ? "bg-blue-200 bg-opacity-30" : "",
+                type === "secondary"
+                    ? "bg-blue-300 bg-opacity-30 hover:bg-opacity-100 transition duration-500 ease-in-out"
+                    : "",
                 className,
             ].join(" ")}
             onClick={onClick}
+            disabled={disabled}
         >
             {children}
         </button>
