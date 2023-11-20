150b6ae9fe1654a6df4eb0d5aca545f1378ecf0b
kacperjanus <kacperjanus8@gmail.com>
2023-11-21 00:46:51 +0100

    Changed the way Top Movers data is fetched

--- src/features/dashboard/useTopMovers.js
+++ src/features/dashboard/useTopMovers.js
@@ -1,16 +1,17 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getTopMovers as getTopMoversApi } from "../../services/apiStockData";
 
 export function useTopMovers() {
-    const queryClient = useQueryClient();
-
-    const { mutate: getTopMovers, isLoading } = useMutation({
-        mutationFn: () => getTopMoversApi(),
-        onSuccess: (data) => queryClient.setQueryData(["topMovers"], data),
-        onError: (error) => {
-            throw new Error(error);
-        },
+    const {
+        isLoading,
+        data = {},
+        error,
+    } = useQuery({
+        queryKey: ["topMovers"],
+        queryFn: () => getTopMoversApi(),
     });
 
-    return { getTopMovers, isLoading };
+    if (error) throw new Error(error);
+
+    return { data, isLoading };
 }
