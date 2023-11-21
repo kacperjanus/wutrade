774588674e1ce5fb5ebaa0258e810431cfb616ab
kacperjanus <kacperjanus8@gmail.com>
2023-11-22 00:00:44 +0100

    Added Page Not Found component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import ContentBox from "../ui/ContentBox";
+
+function NotFound() {
+    return (
+        <ContentBox>
+            <h1 className="uppercase text-center font-medium text-3xl">
+                Page not found!
+            </h1>
+        </ContentBox>
+    );
+}
+
+export default NotFound;
