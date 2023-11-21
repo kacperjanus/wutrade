771b38c98c52f19cbc4965cb6a61931552246656
kacperjanus <kacperjanus8@gmail.com>
2023-11-22 00:56:30 +0100

    Added API to manage data in watchlist table

---
+++ src/services/apiWatchlist.js
@@ -0,0 +1,22 @@
+import supabase from "./supabase";
+
+export async function addToWatchlist({ userId, stockId }) {
+    const { data, error } = await supabase
+        .from("watchlist")
+        .insert([{ user_id: userId, stockId: stockId }])
+        .select();
+
+    if (error) throw new Error(error);
+
+    return data;
+}
+
+export async function removeFromWatchlist({ userId, stockId }) {
+    const { error } = await supabase
+        .from("watchlist")
+        .delete()
+        .eq("stockId", stockId)
+        .eq("user_id", userId);
+
+    if (error) throw new Error(error);
+}
