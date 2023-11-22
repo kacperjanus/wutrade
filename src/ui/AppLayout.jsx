8bb7c977f3ba70930a4ab22187699d68b5b7f599
kacperjanus <kacperjanus8@gmail.com>
2023-11-23 00:06:02 +0100

    Moved fetching user's data to AppLayout component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { useQueryClient } from "@tanstack/react-query";
 
 function AppLayout() {
+    const queryClient = useQueryClient();
+
+    const user = queryClient.getQueryData(["user"]);
     return (
         <div className="grid grid-cols-[26rem,_1fr] grid-rows-[auto,1fr] h-screen">
-            <Header />
+            <Header user={user} />
             <Sidebar />
             <main className="bg-black overflow-scroll pt-[2.4rem] px-[4.8rem] pb-[6.4rem]">
                 <div className="flex flex-col gap-[3.2rem] max-w-[120rem] my-0 mx-auto">
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,26 +1,21 @@
-import { useQueryClient } from "@tanstack/react-query";
 import { useSignOut } from "../features/authentication/useSignout";
 import PrimaryButton from "./Button";
 import { formatCurrency } from "../utils/helpers";
 import FullSpinner from "./FullSpinner";
 import SeachBar from "./SearchBar";
-import { useEffect } from "react";
 
-function Header() {
+function Header({ user }) {
     const { signOut, isLoading } = useSignOut();
-    const queryClient = useQueryClient();
-
-    const data = queryClient.getQueryData(["user"]);
 
     return (
         <header className="bg-[#282828] py-5 px-14 gap-10 border-b-1 items-center justify-end grid grid-cols-[1fr,1fr,auto,auto]">
             {isLoading ? <FullSpinner /> : ""}
             <h1 className="text-white">
-                Hello {data?.user_metadata.firstName}
+                Hello {user?.user_metadata.firstName}
             </h1>
             <SeachBar />
             <h1 className="text-white">
-                Balance: {formatCurrency(data?.user_metadata.balance)}
+                Balance: {formatCurrency(user?.user_metadata.balance)}
             </h1>
             <PrimaryButton type="secondary" onClick={() => signOut()}>
                 Log out
