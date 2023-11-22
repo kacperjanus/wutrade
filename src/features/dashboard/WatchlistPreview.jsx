363fa67135d9c41ca905b31909f115713f6e7d88
kacperjanus <kacperjanus8@gmail.com>
2023-11-23 00:07:13 +0100

    Connected component to watchlist query

--- src/features/dashboard/WatchlistPreview.jsx
+++ src/features/dashboard/WatchlistPreview.jsx
@@ -1,7 +1,21 @@
 import ContentBox from "../../ui/ContentBox";
+import { useWatchlist } from "../watchlist/useWatchlist";
+import WatchlistItem from "../watchlist/watchlistItem";
 
 function WatchlistPreview() {
-    return <ContentBox>Watchlist</ContentBox>;
+    const { data, isLoading } = useWatchlist();
+    data.slice(0, 5);
+    console.log(data);
+    return (
+        <ContentBox>
+            Watchlist
+            <ul>
+                {data.map((item, i) => (
+                    <WatchlistItem item={item} key={i} />
+                ))}
+            </ul>
+        </ContentBox>
+    );
 }
 
 export default WatchlistPreview;
