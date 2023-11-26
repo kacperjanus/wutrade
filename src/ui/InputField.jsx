1c4a288c81e6864c06a5f2492a7b8a91caf8f55b
kacperjanus <kacperjanus8@gmail.com>
2023-11-26 18:24:13 +0100

    Added disabled, value, and onChange props

--- src/ui/InputField.jsx
+++ src/ui/InputField.jsx
@@ -4,14 +4,20 @@ function InputField({
     defaultValue,
     type,
     registerFunction,
+    disabled,
+    value,
+    onChangeFunction = () => {},
 }) {
     return (
         <div className="flex flex-col">
             <label className="ml-2 text-xl">{label}</label>
             <input
-                className="bg-slate-700 focus:outline-0 bg-opacity-50 rounded-full px-4 py-2 focus:shadow-lg focus:shadow-sky-300/20"
+                className="bg-slate-700 disabled:cursor-not-allowed disabled:bg-slate-400 focus:outline-0 bg-opacity-50 rounded-full px-4 py-2 focus:shadow-lg focus:shadow-sky-300/20"
                 defaultValue={defaultValue}
                 type={type}
+                disabled={disabled}
+                value={value}
+                onInput={(e) => onChangeFunction(e)}
                 {...registerFunction(fieldName)}
             />
         </div>
