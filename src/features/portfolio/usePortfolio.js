5831e742ae80bc844de37a11d35a8dfd16a9b0ab
kacperjanus <kacperjanus8@gmail.com>
2023-11-27 23:58:32 +0100

    Added custom hook for retreiving logged user's portfolio

---
+++ src/features/portfolio/usePortfolio.js
@@ -0,0 +1,29 @@
+import { useQueryClient } from "@tanstack/react-query";
+
+export function usePortfolio() {
+    //Pull transaction data
+    const queryClient = useQueryClient();
+    const transactions = queryClient.getQueryData(["transactions"]);
+
+    if (!transactions) return;
+
+    //Get unique companies in the transaction history
+    const companies = [];
+    const values = Object.values(transactions);
+    for (let i = 0; i < values.length; i++)
+        if (!companies.includes(values[i]?.stockId))
+            companies.push(values[i].stockId);
+
+    //Create porfolio by summing up all transations related to single company in company array
+    const portfolio = [];
+    companies.forEach((company) => {
+        const portfolioItem = { company: company, noShares: 0 };
+        for (let i = 0; i < values.length; i++)
+            if (company === values[i].stockId)
+                portfolioItem.noShares += values[i].quantity;
+
+        portfolio.push(portfolioItem);
+    });
+
+    return queryClient.setQueryData(["portfolio"], portfolio);
+}
