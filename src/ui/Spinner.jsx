e832cb42a9acf83b3ecb9df0355a2ad739d8eedc
kacperjanus <kacperjanus8@gmail.com>
2023-11-29 20:21:49 +0100

    Centered the Spinner

--- src/ui/Spinner.jsx
+++ src/ui/Spinner.jsx
@@ -2,7 +2,7 @@ import ContentBox from "./ContentBox";
 
 function Spinner() {
     return (
-        <ContentBox>
+        <ContentBox className="flex items-center justify-center">
             <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600  top-2/4 left-2/4" />
         </ContentBox>
     );
