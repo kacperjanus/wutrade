bec176ed70ded8ab1da640af8f444e5a6152ca53
kacperjanus <kacperjanus8@gmail.com>
2023-11-30 22:30:13 +0100

    Added Volume line and fixed Tooltip styling

--- src/features/stocks/PriceGraph.jsx
+++ src/features/stocks/PriceGraph.jsx
@@ -1,5 +1,6 @@
 import {
     CartesianGrid,
+    Legend,
     Line,
     LineChart,
     ResponsiveContainer,
@@ -16,29 +17,68 @@ function PriceGraph({ prices }) {
         .map((item) => item["1. open"])
         .reverse();
 
+    const volume = Object.values(prices["Time Series (5min)"])
+        .map((item) => item["5. volume"])
+        .reverse();
+
     const data = times.map((item, i) => {
-        return { time: item, price: Number(price[i]) };
+        return {
+            time: item,
+            price: Number(price[i]),
+            volume: Number(volume[i]),
+        };
     });
 
     //TODO make reference line when the day changes within graph domain
 
     return (
-        <ResponsiveContainer width="100%" height={300}>
-            <LineChart data={data} margin={{ bottom: 30 }}>
-                <CartesianGrid strokeDasharray="4" />
-                <XAxis dataKey="time" angle={290} tickSize={20} />
-                <YAxis unit="$" domain={["auto", "auto"]} />
-                <Tooltip />
-                <Line
-                    type="monotone"
-                    dataKey="price"
-                    stroke="#0ea5e9"
-                    name="Price"
-                    unit="$"
-                    dot={false}
-                />
-            </LineChart>
-        </ResponsiveContainer>
+        <div className="mt-3">
+            <ResponsiveContainer width="100%" height={300}>
+                <LineChart
+                    data={data}
+                    margin={{ bottom: 30, left: 30, right: 30 }}
+                >
+                    <CartesianGrid strokeDasharray="5 5" vertical={false} />
+                    <XAxis dataKey="time" angle={290} tickSize={20} />
+                    <YAxis
+                        dataKey="price"
+                        yAxisId="left"
+                        orientation="left"
+                        unit="$"
+                        domain={["auto", "auto"]}
+                        allowDataOverflow={true}
+                    />
+                    <YAxis
+                        dataKey="volume"
+                        yAxisId="right"
+                        orientation="right"
+                        domain={["auto", "auto"]}
+                    />
+                    <Tooltip contentStyle={{ backgroundColor: "black" }} />
+                    <Legend verticalAlign="top" />
+                    <Line
+                        type="monotone"
+                        dataKey="price"
+                        stroke="#0ea5e9"
+                        name="Price"
+                        unit="$"
+                        dot={false}
+                        strokeWidth={2}
+                        yAxisId="left"
+                    />
+                    <Line
+                        type="monotone"
+                        dataKey="volume"
+                        stroke="#0ea"
+                        name="Volume"
+                        unit=" units"
+                        yAxisId="right"
+                        dot={false}
+                        strokeWidth={2}
+                    />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
     );
 }
 
