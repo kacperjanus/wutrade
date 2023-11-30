88eaf1a6eece45aea6dd6f7655290ffd36d1cacf
kacperjanus <kacperjanus8@gmail.com>
2023-11-30 01:00:57 +0100

    Added Logo to the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,11 @@
 import { Link } from "react-router-dom";
+import Logo from "../ui/Logo";
 
 function Homepage() {
     return (
         <div className="flex flex-row">
-            <div className="bg-black flex flex-col h-screen w-full text-white justify-center items-center ">
-                <h2 className="text-3xl">WUTrade</h2>
-                <h2 className="text-white">
-                    Your Virtual Stock Market Application
-                </h2>
+            <div className="bg-black flex flex-col h-screen w-full text-white justify-center items-center">
+                <Logo />
             </div>
             <div className="bg-[#282828] h-screen w-full flex flex-col justify-center items-center">
                 <Link
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,7 +1,7 @@
 function Logo() {
     return (
         <div className="flex justify-center content-center">
-            <img width={200} alt="WUTrade logo" src="/wutrade-logo.png"></img>;
+            <img width={200} alt="WUTrade logo" src="/wutrade-logo.png"></img>
         </div>
     );
 }
