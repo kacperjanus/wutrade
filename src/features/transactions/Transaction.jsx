fa43f683abed0b50d04cc0b62abacb5efaf15a17
kacperjanus <kacperjanus8@gmail.com>
2023-12-02 23:44:47 +0100

    Code refactoring and formatting date

--- src/features/transactions/Transaction.jsx
+++ src/features/transactions/Transaction.jsx
@@ -1,20 +1,17 @@
+import { format } from "date-fns";
+
 function Transaction({ data, number }) {
-    console.log(data);
+    const buy = data.quantity > 0;
+
     return (
         <li>
             <div className="my-1">
                 {number}.{" "}
-                {data.quantity > 0 ? (
-                    <span className="text-green-500">
-                        Bought {data.quantity} shares of {data.stockId} on
-                        {data.createdAt}
-                    </span>
-                ) : (
-                    <span className="text-red-500">
-                        Sold {data.quantity * -1} shares of {data.stockId} on{" "}
-                        {data.createdAt}
-                    </span>
-                )}
+                <span className={`text-${buy ? "green" : "red"}-500`}>
+                    {buy ? "Bought" : "Sold"} {data.quantity} shares of{" "}
+                    {data.stockId} on{" "}
+                    {format(new Date(data.createdAt), "dd MMMM yyyy H:m")}
+                </span>
             </div>
         </li>
     );
