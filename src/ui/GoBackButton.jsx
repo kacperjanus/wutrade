45fcae9ef0176e9e68fc55aaac7e54f8a6f63233
kacperjanus <kacperjanus8@gmail.com>
2023-12-07 23:49:11 +0100

    Replaced text with an icon

--- src/ui/GoBackButton.jsx
+++ src/ui/GoBackButton.jsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { HiOutlineArrowCircleLeft } from "react-icons/hi";
 
 function GoBackButton() {
     const navigate = useNavigate();
     return (
         <div className="absolute mt-4 ml-4" onClick={() => navigate(-1)}>
-            <Button type="secondary">GO BACK</Button>
+            <Button type="secondary">
+                <HiOutlineArrowCircleLeft color="white" size={25} />
+            </Button>
         </div>
     );
 }
