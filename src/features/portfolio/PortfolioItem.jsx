054bc7a3e339124ae66a12fea3024b966a90d8a3
kacperjanus <kacperjanus8@gmail.com>
2023-12-08 00:25:58 +0100

    Updated loading state

--- src/features/portfolio/PortfolioItem.jsx
+++ src/features/portfolio/PortfolioItem.jsx
@@ -8,14 +8,14 @@ function PortfolioItem({ company }) {
     const price =
         data?.["Global Quote - DATA DELAYED BY 15 MINUTES"]?.["02. open"];
 
-    return isLoading ? (
-        <p>Loading...</p>
-    ) : (
+    return (
         <li className="p-2">
             <div className="flex flex-row justify-between">
                 {company.company} - {company.noShares} shares -{" "}
-                {formatCurrency(Number(price).toFixed(2))} per share
-                <BuySellButtons company={company.company} price={10000} />
+                {isLoading
+                    ? "Loading..."
+                    : `${formatCurrency(Number(price).toFixed(2))} per share`}
+                <BuySellButtons company={company.company} />
                 <GoToStockButton company={company.company} />
             </div>
         </li>
