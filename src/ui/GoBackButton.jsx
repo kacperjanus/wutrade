afba35d6ce9430901c90d055509f9a4c21d052fc
kacperjanus <kacperjanus8@gmail.com>
2023-12-12 23:14:20 +0100

    Changed go back destination

--- src/ui/GoBackButton.jsx
+++ src/ui/GoBackButton.jsx
@@ -5,7 +5,7 @@ import { HiOutlineArrowCircleLeft } from "react-icons/hi";
 function GoBackButton() {
     const navigate = useNavigate();
     return (
-        <div className="absolute mt-4 ml-4" onClick={() => navigate(-1)}>
+        <div className="absolute mt-4 ml-4" onClick={() => navigate("/")}>
             <Button type="secondary">
                 <HiOutlineArrowCircleLeft color="white" size={25} />
             </Button>
