651e7d9c8caf27a94338c43d128f752094872d81
kacperjanus <kacperjanus8@gmail.com>
2023-12-14 01:51:37 +0100

    Changed Transaction styling

--- src/features/transactions/Transaction.jsx
+++ src/features/transactions/Transaction.jsx
@@ -7,12 +7,14 @@ function Transaction({ data, number }) {
 
     return (
         <li>
-            <div className="my-1">
+            <div className="my-2 rounded-md bg-slate-700 bg-opacity-40 py-2 px-5 hover:shadow-lg">
                 {number}.{" "}
                 <span className={`text-${buy ? "green" : "red"}-500`}>
                     {buy ? "Bought" : "Sold"} {Math.abs(data.quantity)} shares
                     of{" "}
-                    <Link to={`/explore/${data.stockId}`}>{data.stockId}</Link>{" "}
+                    <Link className="font-bold" to={`/explore/${data.stockId}`}>
+                        {data.stockId}
+                    </Link>{" "}
                     on {format(new Date(data.createdAt), "dd MMMM yyyy H:m")} at{" "}
                     {formatCurrency(data.pricePerShare)} per share
                 </span>
