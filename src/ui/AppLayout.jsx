1aae291a3701b1bd0d11478d41cbcda0e4edb127
kacperjanus <kacperjanus8@gmail.com>
2023-12-17 00:16:43 +0100

    AppLayout is now pulling all transactions

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,17 +5,25 @@ import { useUser } from "../features/authentication/useUser";
 import PageLayout from "./PageLayout";
 import { useTransactions } from "../features/transactions/useTransactions";
 import Spinner from "./Spinner";
+import { useAllTransactions } from "../features/transactions/useAllTransactions";
 
 function AppLayout() {
     const { user } = useUser();
     //TODO try to find better solution for loading transactions first
     const { isLoading } = useTransactions();
+    const { isLoading: isLoadingAllTransactions } = useAllTransactions();
     return (
         <div className="grid grid-cols-[26rem,_1fr] grid-rows-[auto,1fr] h-screen">
             <Header user={user} />
             <Sidebar />
             <main className="bg-black overflow-scroll pt-[2.4rem] px-[4.8rem] pb-[6.4rem]">
-                <PageLayout>{isLoading ? <Spinner /> : <Outlet />}</PageLayout>
+                <PageLayout>
+                    {isLoading || isLoadingAllTransactions ? (
+                        <Spinner />
+                    ) : (
+                        <Outlet />
+                    )}
+                </PageLayout>
             </main>
         </div>
     );
