e56937735fd26f17819a0b26f4c5aeab3fbd99a1
kacperjanus <kacperjanus8@gmail.com>
2023-12-20 02:25:18 +0100

    Added loading PortfolioValues

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,6 +6,7 @@ import PageLayout from "./PageLayout";
 import { useTransactions } from "../features/transactions/useTransactions";
 import Spinner from "./Spinner";
 import { useAllTransactions } from "../features/transactions/useAllTransactions";
+import { usePortfolioValues } from "../features/portfolio/usePortfolioValues";
 
 function AppLayout() {
     const {
@@ -15,12 +16,17 @@ function AppLayout() {
     //TODO try to find better solution for loading transactions first
     const { isLoading } = useTransactions();
     const { isLoading: isLoadingAllTransactions } = useAllTransactions();
+    const { isLoading: isLoadingPortfolioValues } = usePortfolioValues();
+
     return (
         <div className="grid grid-cols-[26rem,_1fr] grid-rows-[auto,1fr] h-screen">
             <Header user={userMetadata} />
             <Sidebar />
             <main className="bg-black overflow-scroll pt-[2.4rem] px-[4.8rem] pb-[6.4rem]">
-                {isLoading || isLoadingAllTransactions || isLoadingUser ? (
+                {isLoading ||
+                isLoadingAllTransactions ||
+                isLoadingPortfolioValues ||
+                isLoadingUser ? (
                     <Spinner />
                 ) : (
                     <PageLayout>
