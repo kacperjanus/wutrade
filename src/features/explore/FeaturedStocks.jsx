7444ea74466959ab77199c278f3bf7c4c1fe6976
kacperjanus <kacperjanus8@gmail.com>
2024-01-02 20:11:54 +0100

    Added more featured stocks

--- src/features/explore/FeaturedStocks.jsx
+++ src/features/explore/FeaturedStocks.jsx
@@ -4,14 +4,26 @@ import FeaturedStock from "./FeaturedStock";
 function FeaturedStocks() {
     return (
         <ContentBox>
-            <span className="font-bold">Featured stocks</span>
-            <div className={"flex flex-row gap-2"}>
-                <FeaturedStock stockId="AAPL">Apple Inc.</FeaturedStock>
-                <FeaturedStock stockId="SNAP">Snap Inc.</FeaturedStock>
-                <FeaturedStock stockId="TSLA">Tesla</FeaturedStock>
-                <FeaturedStock stockId="MSFT">Microsoft</FeaturedStock>
-                <FeaturedStock stockId="KO">Coca-cola</FeaturedStock>
-                <FeaturedStock stockId="NKE">Nike</FeaturedStock>
+            <div className="font-bold mb-3">Featured stocks</div>
+            <div className={"flex flex-row text-center gap-2 flex-wrap"}>
+                <FeaturedStock stockId="AAPL">AAPL - Apple Inc.</FeaturedStock>
+                <FeaturedStock stockId="SNAP">SNAP - Snap Inc.</FeaturedStock>
+                <FeaturedStock stockId="TSLA">TSLA - Tesla</FeaturedStock>
+                <FeaturedStock stockId="MSFT">MSFT - Microsoft</FeaturedStock>
+                <FeaturedStock stockId="KO">KO - Coca-cola</FeaturedStock>
+                <FeaturedStock stockId="TTWO">
+                    TTWO - Take Two Interactive
+                </FeaturedStock>
+                <FeaturedStock stockId="T">T - AT&T</FeaturedStock>
+                <FeaturedStock stockId="JNJ">
+                    JNJ - Johnson & Johnson
+                </FeaturedStock>
+                <FeaturedStock stockId="NVDA">NVDA - Nvidia</FeaturedStock>
+                <FeaturedStock stockId="NFLX">NFLX - Netflix</FeaturedStock>
+                <FeaturedStock stockId="PYPL">PYPL - PayPal</FeaturedStock>
+                <FeaturedStock stockId="DIS">DIS - Walt Disney</FeaturedStock>
+                <FeaturedStock stockId="ADBE">ADBE - Adobe</FeaturedStock>
+                <FeaturedStock stockId="PINS">PINS - Pinterest</FeaturedStock>
             </div>
         </ContentBox>
     );
