7031db8dd01bd60abd8724fefe25d120e2ea9900
kacperjanus <kacperjanus8@gmail.com>
2024-01-08 22:27:01 +0100

    Added test id prop

--- src/ui/ContentBox.jsx
+++ src/ui/ContentBox.jsx
@@ -1,7 +1,8 @@
-function ContentBox({ className, children }) {
+function ContentBox({ className, dataTestId, children }) {
     return (
         <div
             className={`bg-[#282828] h-auto w-full mx-aut py-[30px] px-10 text-white rounded-sm ${className}`}
+            data-testid={dataTestId}
         >
             {children}
         </div>
